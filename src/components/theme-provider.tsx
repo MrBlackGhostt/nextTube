199dea8ee6f64f067945c5f9a0ea6feecae310ce
MrBlackGhostt <hemantkumar4213@icloud.com>
2024-09-03 22:38:53 +0530

    add the theme toogle

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,7 +2,14 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
