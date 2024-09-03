65d75211fae969ace2ccc5a6d6ee519883c306b4
ASHISH MOHANTY <ashishmohaty10@gmail.com>
2024-09-03 18:14:58 +0530

    added light and dark-mode

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -67,14 +67,17 @@ const Navbar = ({ children }: NavbarProps) => {
   return (
     <>
       {/* Navbar */}
-      <nav className="flex justify-between items-center p-2 bg-white border-b border-gray-200 dark:bg-slate-950">
+      <nav className="flex justify-between items-center p-2 bg-white border-b border-gray-200 dark:bg-black">
         {/* Left section */}
         <div className="flex items-center">
           <button
-            className="p-2 hover:bg-gray-100 rounded-full"
+            className="p-2 hover:bg-gray-100 rounded-full group"
             onClick={() => setIsSidebarOpen((prev) => !prev)}
           >
-            <FaBars size={20} color="#606060" />
+            <FaBars
+              size={20}
+              className="dark:text-slate-200 dark:group-hover:text-slate-800"
+            />
           </button>
           <div className="flex items-center ml-4">
             <Image
@@ -157,26 +160,32 @@ const Navbar = ({ children }: NavbarProps) => {
           <div className={`flex flex-col mb-4`}>
             <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 rounded-lg">
               <span className="mr-4">
-                <FaHome size={20} />
+                <FaHome size={20} className="dark:text-slate-200" />
               </span>
               {isSidebarOpen && (
-                <span className="text-sm font-medium">Home</span>
+                <span className="text-sm font-medium dark:text-slate-200">
+                  Home
+                </span>
               )}
             </button>
             <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 rounded-lg">
               <span className="mr-4">
-                <MdSubscriptions size={20} />
+                <MdSubscriptions size={20} className="dark:text-slate-200" />
               </span>
               {isSidebarOpen && (
-                <span className="text-sm font-medium">Subscriptions</span>
+                <span className="text-sm font-medium dark:text-slate-200">
+                  Subscriptions
+                </span>
               )}
             </button>
             <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 rounded-lg">
               <span className="mr-4">
-                <FaPlay size={20} />
+                <FaPlay size={20} className="dark:text-slate-200" />
               </span>
               {isSidebarOpen && (
-                <span className="text-sm font-medium">Playlist</span>
+                <span className="text-sm font-medium dark:text-slate-200">
+                  Playlist
+                </span>
               )}
             </button>
           </div>
@@ -186,27 +195,35 @@ const Navbar = ({ children }: NavbarProps) => {
             <div className="flex flex-col mb-4 border-t border-gray-200 pt-4">
               <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 rounded-lg">
                 <span className="mr-4">
-                  <FaHistory size={20} />
+                  <FaHistory size={20} className="dark:text-slate-200" />
                 </span>
-                <span className="text-sm font-medium">History</span>
+                <span className="text-sm font-medium dark:text-slate-200">
+                  History
+                </span>
               </button>
               <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 rounded-lg">
                 <span className="mr-4">
-                  <RiVideoLine size={20} />
+                  <RiVideoLine size={20} className="dark:text-slate-200" />
                 </span>
-                <span className="text-sm font-medium">Your Videos</span>
+                <span className="text-sm font-medium dark:text-slate-200">
+                  Your Videos
+                </span>
               </button>
               <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 rounded-lg">
                 <span className="mr-4">
-                  <FaClock size={20} />
+                  <FaClock size={20} className="dark:text-slate-200" />
                 </span>
-                <span className="text-sm font-medium">Watch Later</span>
+                <span className="text-sm font-medium dark:text-slate-200">
+                  Watch Later
+                </span>
               </button>
               <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 rounded-lg">
                 <span className="mr-4">
-                  <FaThumbsUp size={20} />
+                  <FaThumbsUp size={20} className="dark:text-slate-200" />
                 </span>
-                <span className="text-sm font-medium">Liked Videos</span>
+                <span className="text-sm font-medium dark:text-slate-200">
+                  Liked Videos
+                </span>
               </button>
             </div>
           )}
