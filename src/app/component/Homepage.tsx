a344087298d8c4d9f20a56be50e90a5946daf4df
MrBlackGhostt <hemantkumar4213@icloud.com>
2024-09-05 22:18:43 +0530

    add the home page as starting and mobile have the sidebar and add little responsiveness in the home

--- src/app/component/Homepage.tsx
+++ src/app/component/Homepage.tsx
@@ -1,14 +1,8 @@
-import Link from "next/link";
-
-
 
 const Homepage= () => {
 
   return (
-    <div className="p-4 border-2 border-green-400">
-      <h1>Fetch the data</h1>
-     <Link href={'home'}>GO to Home</Link>
-    </div>
+   <Homepage/>
   );
 };
 
