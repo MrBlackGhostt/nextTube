34362b5607bd3dd9830fa0179e60d29273eaf142
Harsh Gupta <148535343+Harsh97563@users.noreply.github.com>
2024-09-15 11:50:14 +0530

    Revert "Develop"

--- src/app/component/Homepage.tsx
+++ src/app/component/Homepage.tsx
@@ -1,8 +1,6 @@
 
 const Homepage= () => {
 
-
-
   return (
    <Homepage/>
   );
--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -29,10 +29,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { useRouter, useSearchParams } from "next/navigation";
 
-
-import { GetSubscribtionData} from "../api/auth/youtubeapi";
 import { GetVideoById } from "../api/auth/youtubeapi";
-
 import { ModeToggle } from "@/components/theme-toggler";
 import { ReactNode, useEffect, useState } from "react";
 import Link from "next/link";
@@ -45,46 +42,8 @@ interface NavbarProps {
 const Navbar = ({ children }: NavbarProps) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
   const { data: session, status } = useSession();
-  const [mobileSearch, setMobileSearch] = useState<boolean>(false)
-  const [userSubscribedChannels, setuserSubscribedChannels] = useState<Channel[]>([])
-  const [apiKey, setApiKey] = useState("");
-
-  const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // Store API key in localStorage
-    localStorage.setItem("userApiKey", apiKey);
-    alert("API key saved successfully.");
-  };
-
+const [mobileSearch, setMobileSearch] = useState<boolean>(false)
   const router = useRouter();
-
-  const dummySubscriptions = [
-    {
-      id: 1,
-      title: 'freeCodeCamp.org',
-      thumbnail: 'https://yt3.ggpht.com/ytc/AIdro_lGRc-05M2OoE1ejQdxeFhyP7OkJg9h4Y-7CK_5je3QqFI=s88-c-k-c0x00ffffff-no-rj',
-    },
-    {
-      id: 2,
-      title: 'Dhruv Rathee',
-      thumbnail: 'https://yt3.ggpht.com/oQ2NQMuq-9IS6_MZdsGXa0RWecaACremA01Z7jo-YpoEF1H6PyUF8PZzXkV10OYwSP3UMJDeTg=s88-c-k-c0x00ffffff-no-rj',
-    },
-    {
-      id: 3,
-      title: 'Beebom',
-      thumbnail: 'https://yt3.ggpht.com/fQQ_gL8K48zFYTIT4F-agu_Snt4wQbpPfXbfwdOaRgu0DVRwmB_r6V60JXtW-6FfphauTmssWA=s88-c-k-c0x00ffffff-no-rj',
-    },
-  ];
-
-
-  if(session?.user && session?.accessToken) {
-  ( async ()=>{
-  const response = await GetSubscribtionData(session.accessToken);
-  if(response) setuserSubscribedChannels(response.items);
-   
-  })()
-    
-  }
 
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -108,9 +67,7 @@ const Navbar = ({ children }: NavbarProps) => {
   return (
     <>
       {/* Navbar */}
-
-      <nav className="flex justify-between items-center sticky top-0 z-50 p-2 bg-white dark:bg-black">
-
+      <nav className="flex justify-between items-center sticky top-0 z-10 p-2 dark:bg-black">
         {/* Left section */}
         {mobileSearch && (
   <div className="absolute top-0 left-0 flex items-center justify-between w-full py-2 z-50 bg-white dark:bg-black shadow-md">
@@ -141,16 +98,12 @@ const Navbar = ({ children }: NavbarProps) => {
 
         <div className="flex items-center justify-between">
           <button
-
-            className="p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-full group"
-
-
+            className="p-2 hover:bg-gray-100 rounded-full group"
             onClick={() => setIsSidebarOpen((prev) => !prev)}
           >
             <FaBars
               size={20}
-
-              className="dark:text-slate-200 dark:group-hover:text-slate-100"
+              className="dark:text-slate-200 dark:group-hover:text-slate-800"
             />
           </button>
           <div className="flex relative md:w-12 md:h-12  items-center ml-6">
@@ -172,13 +125,11 @@ const Navbar = ({ children }: NavbarProps) => {
               onChange={(e) =>
                 setSearchTerm(e.target.value)}
                 onKeyDown={handleKeyDown}
-
-              className="w-full px-4 py-2 text-gray-700 bg-gray-100 rounded-l-full focus:outline-none focus:border-blue-500 dark:bg-zinc-800 dark:text-slate-200"
+              className="w-full px-4 py-2 text-gray-700 bg-gray-100 border border-gray-300 rounded-l-full focus:outline-none focus:border-blue-500 dark:bg-zinc-800 dark:text-slate-200"
             />
             <button
               onClick={(e) => handleSearch(e)}
-              className="px-6 py-2 bg-gray-100 dark:bg-zinc-800 rounded-r-full"
-
+              className="px-6 py-2 bg-gray-100 border border-l-0 border-gray-300 rounded-r-full hover:bg-gray-200"
             >
               <FaSearch size={24} color="#606060" />
             </button>
@@ -189,10 +140,8 @@ const Navbar = ({ children }: NavbarProps) => {
       
               <FaSearch className="md:hidden" size={24} color="#606060" onClick={()=>setMobileSearch(!mobileSearch)}/>
           
-
-          <button className="hidden md:flex p-2 hover:bg-gray-100  dark:hover:bg-gray-800 rounded-full">
-            <FaBell size={20} className="text-slate-800 dark:text-slate-200" />
-
+          <button className="hidden md:flex p-2  hover:bg-gray-100 rounded-full">
+            <FaBell size={20} className=" dark:text-slate-200 text-slate-800" />
           </button>
           <ModeToggle />
 <div className="hidden md:flex">
@@ -200,9 +149,7 @@ const Navbar = ({ children }: NavbarProps) => {
           {status != "authenticated" ? (
             <div >
               <button
-
-                className="p-2 mx-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-full flex items-center dark:text-slate-200 dark:hover:text-slate-100"
-
+                className="p-2 mx-2 hover:bg-gray-100 rounded-full flex items-center dark:text-slate-200 dark:hover:text-slate-800"
                 onClick={() => signIn("google")}
               >
                 <span className="">Login</span>
@@ -235,111 +182,44 @@ const Navbar = ({ children }: NavbarProps) => {
 
       {/* Sidebar */}
       <div className="flex">
-
-      <div
-        className={`fixed md:relative z-10 flex flex-col h-full p-4 transition-transform duration-300 ${
-          isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
-        } md:translate-x-0 ${isSidebarOpen ? 'w-2/5 md:w-fit' : 'md:w-fit'}`}
-      >
-        {/* Top section */}
-        <div className={`flex flex-col mb-4`}>
-          <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg">
-            <span className="mr-4">
-              <FaHome size={20} className="dark:text-slate-200" />
-            </span>
-            {isSidebarOpen && (
-              <Link href={'/home'} className="text-sm font-medium dark:text-slate-200">
-                Home
-              </Link>
-            )}
-          </button>
-          <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg">
-            <span className="mr-4">
-              <MdSubscriptions size={20} className="dark:text-slate-200" />
-            </span>
-            {isSidebarOpen && (
-              <Link href={'/subscriptions'} className="text-sm font-medium dark:text-slate-200">
-                Subscriptions
-              </Link>
-            )}
-          </button>
-          <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg">
-            <span className="mr-4">
-              <FaPlay size={20} className="dark:text-slate-200" />
-            </span>
-            {isSidebarOpen && (
-              <Link href={'/playlist'} className="text-sm font-medium dark:text-slate-200">
-                Playlist
-              </Link>
-            )}
-          </button>
-          <div className="flex md:hidden">
-            {status != 'authenticated' ? (
-              <div>
-                <button
-                  className="p-2 mx-2 hover:bg-gray-100 rounded-full flex items-center dark:text-slate-200 dark:hover:text-slate-800"
-                  onClick={() => signIn('google')}
-                >
-                  <span className="">Login</span>
-                </button>
-              </div>
-            ) : (
-              <div>
-                <DropdownMenu>
-                  <DropdownMenuTrigger className=" flex items-center gap-1 w-fit  relative  rounded-full ">
-                    <div className="relative w-8 h-8">
-                      <Image
-                        src={session.user?.image || ''}
-                        alt="user image"
-                        fill
-                        className="h-auto rounded-full"
-                      />
-                    </div>
-                    <div className="text-sm  ">{session.user?.name}</div>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem onClick={() => signOut()}>Logout</DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </div>
-            )}
-          </div>
-        </div>
-
-        {/* Subscriptions Section */}
-        {isSidebarOpen && (
-          <div className="flex flex-col mb-4 border-t border-gray-200 pt-4">
-            <h2 className="text-sm font-semibold mb-2">Subscriptions</h2>
-            {dummySubscriptions.map((subscription) => (
-              <div key={subscription.id} className="flex items-center mb-2 p-2 hover:bg-gray-100 rounded-lg">
-                <Image
-                  src={subscription.thumbnail}
-                  alt={`${subscription.title} thumbnail`}
-                  width={40}
-                  height={40}
-                  className="rounded-full"
-                />
-                <span className="ml-4 text-sm font-medium dark:text-slate-200">{subscription.title}</span>
-              </div>
-            ))}
-          </div>
-        )}
-
-        {/* Middle section */}
-        {isSidebarOpen && (
-          <div className="flex flex-col mb-4 border-t border-gray-200 pt-4">
-            <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg">
+        <div
+          className={`fixed md:relative z-10 flex flex-col h-full p-4 bg-white dark:bg-black  transition-transform duration-300 ${
+            isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+          } md:translate-x-0 ${isSidebarOpen ? "w-2/5 md:w-fit" : "md:w-fit"}`}
+        >
+          {/* Top section */}
+          <div className={`flex flex-col mb-4`}>
+            <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 rounded-lg">
               <span className="mr-4">
-                <FaHistory size={20} className="dark:text-slate-200" />
+                <FaHome size={20} className="dark:text-slate-200" />
               </span>
-              <Link href={'/history'} className="text-sm font-medium dark:text-slate-200">
-                History
-              </Link>
+              {isSidebarOpen && (
+                <Link href={'/home'} className="text-sm font-medium dark:text-slate-200">
+                  Home
+                </Link>
+              )}
             </button>
-          </div>
-        )}
+            <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 rounded-lg">
+              <span className="mr-4">
+                <MdSubscriptions size={20} className="dark:text-slate-200" />
+              </span>
+              {isSidebarOpen && (
+                <Link href={'/subscriptions'} className="text-sm font-medium dark:text-slate-200">
+                  Subscriptions
+                </Link>
+              )}
+            </button>
+            <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 rounded-lg">
+              <span className="mr-4">
+                <FaPlay size={20} className="dark:text-slate-200" />
+              </span>
+              {isSidebarOpen && (
+                <Link href={'/playlist'} className="text-sm font-medium dark:text-slate-200">
+                  Playlist
+                </Link>
+              )}
+            </button>
+            <div className="flex md:hidden">
 
             {status != "authenticated" ? (
             <div>
@@ -377,25 +257,29 @@ const Navbar = ({ children }: NavbarProps) => {
             </div>
           </div>
 
-        )}
-      </div>
-      {isSidebarOpen && (
-        <form onSubmit={handleSubmit}>
-        <label htmlFor="api-key">Enter API Key:</label>
-        <input
-          type="text"
-          id="api-key"
-          name="api-key"
-          value={apiKey}
-          onChange={(e) => setApiKey(e.target.value)}
-          required
-        />
-        <button type="submit">Save API Key</button>
-      </form>
-      )}
-      {children}
-    </div>
+          {/* Middle section */}
+          {isSidebarOpen && (
+            <div className="flex flex-col mb-4 border-t border-gray-200 pt-4">
+              <button className="flex items-center p-2 my-1 text-gray-700 hover:bg-gray-100 rounded-lg">
+                <span className="mr-4">
+                  <FaHistory size={20} className="dark:text-slate-200" />
+                </span>
+                <Link href={'/history'} className="text-sm font-medium dark:text-slate-200">
+                  History
+                </Link>
+              </button>
+            </div>
+          )}
 
+          {/* Bottom section */}
+          {isSidebarOpen && (
+            <div className="flex flex-col border-t border-gray-200 pt-4">
+              {/* Add more links here if needed */}
+            </div>
+          )}
+        </div>
+        {children}
+      </div>
     </>
   );
 };
